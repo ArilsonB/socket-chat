2b6ed5edbc59a459f427bd8cfd6c091db4107fcf
Arilson B. Ferreira <arilsondeveloper@gmail.com>
2024-07-16 20:46:21 -0300

    Updated code

--- apps/web/app/@authenticated/page.tsx
+++ apps/web/app/@authenticated/page.tsx
@@ -5,11 +5,15 @@ const Home = () => {
   const socket = useSocket();
 
   return (
-    <div>
-      <div className="flex h-16 w-full">
+    <div className="flex flex-col h-full">
+      <div className="flex h-16 w-full bg-blue-400">
         <div className="container w-full mx-auto px-4">
-          <h1>Authenticated</h1>
+          <h1 className="font-bold text-white uppercase">Authenticated</h1>
         </div>
+      </div>
+      <div className="flex flex-row h-full">
+        <div className="flex w-72 h-full bg-red-400">aaa</div>
+        <div></div>
       </div>
     </div>
   );
